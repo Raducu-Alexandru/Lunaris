7c676a2ca9552d6b8b9f82ea0b85b56c664e3217
Raducu Alexandru <axel.mircea@gmail.com>
2024-03-23 15:57:17 +0200

    Cleared the git cache

---
+++ src/app/login-module/change-university-page/change-university-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-university-page',
+  templateUrl: './change-university-page.component.html',
+  styleUrl: './change-university-page.component.scss'
+})
+export class ChangeUniversityPageComponent {
+
+}
